dd9b5007751826fbfe9f8730ec6dc9327bb8b324
North Star House <media@thenorthstarhouse.org>
2026-01-07 21:47:29 -0800

    Hardcode Apps Script API URL

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import VolunteerPortal from './VolunteerPortal.jsx';
 
 const App = () => {
-  const apiUrl = import.meta.env.VITE_SHEETS_API_URL || '';
+  const apiUrl =
+    import.meta.env.VITE_SHEETS_API_URL ||
+    'https://script.google.com/macros/s/AKfycbyfPvK6_dfAUIQaAkGvomxXlLqAEUnZXpsprnbj_E_yhSIN162OAUYiJueYu01TykNhdg/exec';
   return <VolunteerPortal apiUrl={apiUrl} />;
 };
 
